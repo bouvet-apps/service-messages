131578e0f1073d7cdf9c675c676b9b2b5d4d6497
Martine Gran <martine.gran@bouvet.no>
2021-06-02 12:24:06 +0200

    fix bug with fetching content from correct branch

--- src/main/resources/services/servicemessage/servicemessage.js
+++ src/main/resources/services/servicemessage/servicemessage.js
@@ -1,5 +1,6 @@
 var portalLib = require('/lib/xp/portal');
 var contentLib = require('/lib/xp/content');
+var contextLib = require('/lib/xp/context');
 var cacheLib = require('/lib/cache');
 
 // Cache for service messages. We will hold them for 60 seconds before
@@ -9,37 +10,43 @@ var dataCache = cacheLib.newCache({
     expire: 60
 });
 
-
 // Get all published content of type servicemessage in the current site.
 var getServiceMessages = function() {
     var sitePath = portalLib.getSite()._path;
     return dataCache.get("serviceMessages-" + sitePath, function () {
-        var result = contentLib.query({
-                sort:   "publish.from DESC",
-                query: " _parentPath LIKE '/content" + sitePath + "*'",
-                branch: "master",
-                contentTypes:  [
-                    app.name + ":servicemessage"
-                ]
-            }
-        ).hits.map(function(hit) {
-            var msg = {};
-            if (hit.displayName) { msg.title = hit.displayName; }
-            if (hit.data.text) { msg.text = hit.data.text; }
-            if (hit.data.link) {
-                msg.link = portalLib.pageUrl( {id: hit.data.link} );
-            }
-            msg.type = hit.data.type;
-            return msg;
-        });
-
+        var result = {};
+        contextLib.run({
+            branch: "master",
+            principals: ["role:system.admin"]
+        }, function () {
+            result = contentLib.query({
+                  sort: "publish.from DESC",
+                  query: " _parentPath LIKE '/content" + sitePath + "*'",
+                  contentTypes: [
+                      app.name + ":servicemessage"
+                  ]
+              }
+            ).hits.map(function (hit) {
+                var msg = {};
+                if (hit.displayName) {
+                    msg.title = hit.displayName;
+                }
+                if (hit.data.text) {
+                    msg.text = hit.data.text;
+                }
+                if (hit.data.link) {
+                    msg.link = portalLib.pageUrl({id: hit.data.link});
+                }
+                msg.type = hit.data.type;
+                return msg;
+            });
+        })
         return result;
     });
 };
 
 // Handle GET requests
 exports.get = function() {
-    log.info("running exports.get");
     var siteConfig = portalLib.getSiteConfig();
     var id = siteConfig.id;
 
--- src/main/resources/site/processors/servicemessage.js
+++ src/main/resources/site/processors/servicemessage.js
@@ -2,8 +2,6 @@ var portalLib = require('/lib/xp/portal');
 var arrayUtil = require('/lib/array-util');
 
 exports.responseProcessor = function(req, res) {
-    log.info("running responseProcessor");
-    log.info(portalLib.serviceUrl({service: 'servicemessage'}));
     var js = '<script src="'+ portalLib.assetUrl({path: 'js/json_parse.js'}) +'"></script>'
            + '<script src="'+ portalLib.assetUrl({path: 'js/scripts.js'}) +'"></script>';
     var html = '<script type="application/json" data-selector="servicemessages">{"service": "' +  portalLib.serviceUrl({service: 'servicemessage'}) + '"}</script>';
